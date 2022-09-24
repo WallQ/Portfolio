570266724b394736b82cda39398bfd24fd308d85
Sérgio Félix <sergio98@outlook.pt>
2022-09-24 07:15:31 +0100

    i18n implementation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+	const { t } = useTranslation();
 	return (
 		<React.Fragment>
-			<footer className='mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8'>
+			<footer className='mx-auto max-w-7xl px-8 py-4'>
 				<div className='flex flex-row items-center justify-between border-t border-gray-300 pt-6'>
 					<div className='flex flex-row items-center justify-between gap-x-4'>
 						<a
-							href='/Portfolio/cookie-policy'
-							className='text-base font-normal text-gray-900'>
-							Cookie Policy
-						</a>
-						<a
-							href='/Portfolio/legal-notice'
-							className='text-base font-normal text-gray-900'>
-							Legal Notice
-						</a>
-						<a
 							href='/Portfolio/privacy-policy'
 							className='text-base font-normal text-gray-900'>
-							Privacy Policy
+							{t('footer.footeritems.item1')}
+						</a>
+						<a
+							href='/Portfolio/terms-conditions'
+							className='text-base font-normal text-gray-900'>
+							{t('footer.footeritems.item2')}
+						</a>
+						<a
+							href='/Portfolio/cookies-policy'
+							className='text-base font-normal text-gray-900'>
+							{t('footer.footeritems.item3')}
 						</a>
 					</div>
 					<small className='text-base font-normal text-gray-900'>
