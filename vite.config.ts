7845f8e8675ca93439cb28474c01007be6025da3
Sérgio Félix <sergio98@outlook.pt>
2022-09-29 00:33:32 +0100

    Vite Plugin PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,62 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [react()],
+	base: '/Portfolio/',
 	server: {
 		port: 3000,
 	},
-	base: '/Portfolio/',
+	plugins: [
+		react(),
+		VitePWA({
+			// registerType: 'autoUpdate',
+			// injectRegister: 'auto',
+			// strategies: 'injectManifest',
+			// includeAssets: ['favicon.svg', 'robots.txt'],
+			// workbox: {
+			// 	globPatterns: [
+			// 		'**/*.{svg,js,css,webp,json,pdf,woff,woff2,png,jpg,html,txt}',
+			// 	],
+			// },
+			manifest: {
+				name: 'Sérgio Félix - Personal Website',
+				short_name: 'Portfolio',
+				description: 'The personal website of Sérgio Félix',
+				// id: '/Portfolio/',
+				// scope: '/Portfolio/',
+				// start_url: '/Portfolio/',
+				theme_color: '#2564eb',
+				background_color: '#ffffff',
+				orientation: 'portrait',
+				display: 'fullscreen',
+				icons: [
+					{
+						src: 'icons/manifest-icon-192.maskable.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'icons/manifest-icon-192.maskable.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+					{
+						src: 'icons/manifest-icon-512.maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any',
+					},
+					{
+						src: 'icons/manifest-icon-512.maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable',
+					},
+				],
+			},
+		}),
+	],
 });
