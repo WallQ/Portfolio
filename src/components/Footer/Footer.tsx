dce8fa842d160867e5a6d8613fd3bf7cb435976e
Sérgio Félix <sergio98@outlook.pt>
2022-09-29 00:36:59 +0100

    Data from sides to center

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,35 +1,63 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
+import RouteCookiesPolicy from '../../routes/CookiesPolicy';
+import RoutePrivacyPolicy from '../../routes/PrivacyPolicy';
+import RouteTermsConditions from '../../routes/TermsConditions';
+
+import LinkedIn from '../../assets/icons/linkedin.svg';
+import GitHub from '../../assets/icons/github.svg';
 
 const Footer: React.FunctionComponent = () => {
 	const { t } = useTranslation();
 	return (
 		<React.Fragment>
 			<footer className='mx-auto max-w-7xl px-8 py-4'>
-				<div className='flex flex-col justify-center gap-y-4'>
-					<hr className='text-gray-300' />
-					<div className='flex flex-row items-center justify-between'>
-						<div className='flex flex-row items-center justify-between gap-x-4'>
-							<a
-								href='/Portfolio/privacy-policy'
-								className='text-base font-normal text-gray-900'>
-								{t('footer.footeritems.item1')}
-							</a>
-							<a
-								href='/Portfolio/terms-conditions'
-								className='text-base font-normal text-gray-900'>
-								{t('footer.footeritems.item2')}
-							</a>
-							<a
-								href='/Portfolio/cookies-policy'
-								className='text-base font-normal text-gray-900'>
-								{t('footer.footeritems.item3')}
-							</a>
-						</div>
-						<small className='text-base font-normal text-gray-900'>
-							&copy; {new Date().getFullYear()} Sérgio Félix
-						</small>
+				<hr className='pb-4 text-gray-300' />
+				<div className='flex flex-col items-center justify-center gap-y-4'>
+					<div className='flex flex-row items-center justify-center gap-x-4'>
+						<a
+							href='https://www.linkedin.com/in/sergiofelixdev/'
+							target='_blank'
+							rel='noreferrer'>
+							<img
+								src={LinkedIn}
+								alt='Linkedin Logo'
+								className='h-6 w-6'
+							/>
+						</a>
+						<a
+							href='https://github.com/WallQ'
+							target='_blank'
+							rel='noreferrer'>
+							<img
+								src={GitHub}
+								alt='GitHub Logo'
+								className='h-6 w-6'
+							/>
+						</a>
 					</div>
+					<div className='flex flex-row items-center justify-center gap-x-4 text-center'>
+						<Link
+							to={RoutePrivacyPolicy.path}
+							className='text-base font-normal text-gray-900'>
+							{t('footer.footeritems.url1')}
+						</Link>
+						<Link
+							to={RouteTermsConditions.path}
+							className='text-base font-normal text-gray-900'>
+							{t('footer.footeritems.url2')}
+						</Link>
+						<Link
+							to={RouteCookiesPolicy.path}
+							className='text-base font-normal text-gray-900'>
+							{t('footer.footeritems.url3')}
+						</Link>
+					</div>
+					<small className='text-base font-normal text-gray-900'>
+						&copy; {new Date().getFullYear()} Sérgio Félix
+					</small>
 				</div>
 			</footer>
 		</React.Fragment>
