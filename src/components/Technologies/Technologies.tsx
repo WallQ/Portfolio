d0721b6be06dc640a7c9094f20073243956b3e97
Sérgio Félix <sergio98@outlook.pt>
2022-09-30 03:08:01 +0100

    Loved Technologies Component

---
+++ src/components/Technologies/Technologies.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Logos from './TechnologiesLogos';
+
+const Technologies: React.FunctionComponent = () => {
+	return (
+		<React.Fragment>
+			<div className='flex flex-col gap-y-8'>
+				<h2 className='text-3xl font-bold capitalize tracking-tight text-gray-900'>
+					Technologies
+				</h2>
+				<div className='grid grid-cols-2 content-center justify-items-center gap-x-4 gap-y-8 md:grid-cols-4 lg:grid-cols-4'>
+					{Logos.map((Icon: string, index: number) => {
+						return (
+							<img
+								key={`Logo-${index}`}
+								src={Icon}
+								alt={`Technology Logo ${index}`}
+								className='h-16 w-16'
+								loading='lazy'
+							/>
+						);
+					})}
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Technologies;
---
+++ src/components/Technologies/TechnologiesLogos.ts
@@ -0,0 +1,19 @@
+import TypeScriptIcon from '../../assets/icons/technologies/typescript.svg';
+import ReactIcon from '../../assets/icons/technologies/reactjs.svg';
+import NextJsIcon from '../../assets/icons/technologies/nextjs.svg';
+import NodeJsIcon from '../../assets/icons/technologies/nodejs.svg';
+import ExpressJsIcon from '../../assets/icons/technologies/expressjs.svg';
+import GraphQLIcon from '../../assets/icons/technologies/graphql.svg';
+import PostgreSQLIcon from '../../assets/icons/technologies/postgresql.svg';
+import TailwindCSSIcon from '../../assets/icons/technologies/tailwindcss.svg';
+
+export default [
+	TypeScriptIcon,
+	ReactIcon,
+	NextJsIcon,
+	NodeJsIcon,
+	ExpressJsIcon,
+	GraphQLIcon,
+	PostgreSQLIcon,
+	TailwindCSSIcon,
+];
