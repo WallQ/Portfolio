6595f9d4816855397eb893c75ab7f145bb31b63d
Sérgio Félix <sergio98@outlook.pt>
2022-09-30 03:01:09 +0100

    Disable ReactDevTools

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from './App';
 import './index.css';
 import './i18n';
 import { registerSW } from 'virtual:pwa-register';
+
+if (process.env.NODE_ENV === 'production') {
+	disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
