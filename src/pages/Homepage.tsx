44e80493d7f5be048bf2dbe87cc6625418f20e0b
Sérgio Félix <sergio98@outlook.pt>
2022-10-10 19:24:06 +0100

    New components for homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 
-import Projects from '../components/Projects/Projects';
+import Experiences from '../components/Experiences/Experiences';
 import Technologies from '../components/Technologies/Technologies';
+import Projects from '../components/Projects/Projects';
+import Contact from '../components/Contact/Contact';
 
 const Homepage: React.FunctionComponent = () => {
 	return (
 		<React.Fragment>
-			<main className='mx-auto flex max-w-7xl flex-col gap-y-8 px-8 py-4'>
-				<Header />
-				<Technologies />
-				<Projects />
+			<main>
+				<div className='mx-auto flex max-w-7xl flex-col gap-y-16 px-8 py-16'>
+					<Header />
+					<Experiences />
+					<Technologies />
+					<Projects />
+				</div>
+				<Contact />
 			</main>
 		</React.Fragment>
 	);
