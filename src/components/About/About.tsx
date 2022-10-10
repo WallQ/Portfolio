8bf649f5720bed3aec01c3e741558a8440c63f07
Sérgio Félix <sergio98@outlook.pt>
2022-10-10 19:24:41 +0100

    New components

---
+++ src/components/About/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About: React.FunctionComponent = () => {
+	return (
+		<React.Fragment>
+			<div className='w-full bg-primary p-8'></div>
+		</React.Fragment>
+	);
+};
+
+export default About;
