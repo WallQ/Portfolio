d9f5ff99992ceb0c57861f418fea806fec6e641c
Sérgio Félix <sergio98@outlook.pt>
2022-10-10 19:22:47 +0100

    Removed react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import AppRoutes from './routes/index';
 
 import Layout from './components/Layout/Layout';
 
-const queryClient = new QueryClient();
-
 const App: React.FunctionComponent = () => {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<Router>
-				<Routes>
-					<Route path='/' element={<Layout />}>
-						{AppRoutes.map(({ path, component: Component }) => {
-							return (
-								<Route
-									key={path}
-									path={path}
-									element={<Component />}
-								/>
-							);
-						})}
-					</Route>
-				</Routes>
-			</Router>
-		</QueryClientProvider>
+		<Router>
+			<Routes>
+				<Route path='/' element={<Layout />}>
+					{AppRoutes.map(({ path, component: Component }) => {
+						return (
+							<Route
+								key={path}
+								path={path}
+								element={<Component />}
+							/>
+						);
+					})}
+				</Route>
+			</Routes>
+		</Router>
 	);
 };
 
